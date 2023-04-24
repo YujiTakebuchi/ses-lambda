7e9eb5343c438e3639b4aab5a20be7fea11400be
YujiTakebuchi <takeyou.swift@gmail.com>
2023-04-25 01:32:44 +0900

    localstack検証用のシェルスクリプトにペイロード用引数追加

--- app/shells/localstack/invoke-func-localstack.sh
+++ app/shells/localstack/invoke-func-localstack.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-awslocal lambda invoke --function-name ses-lambda --region ap-northeast-1 result.log
+while (( $# > 0 ))
+do
+  case $1 in
+    --paylaod | --payload=*)
+      if [[ "$1" =~ ^--paylaod= ]]; then
+        PAYLOAD=$(echo $1 | sed -e 's/^--paylaod=//')
+      elif [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+        echo "'paylaod' requires an argument." 1>&2
+        exit 1
+      else
+        PAYLOAD="--payload $2"
+        shift
+      fi
+      ;;
+  esac
+  shift
+done
+
+awslocal lambda invoke --function-name ses-lambda --region ap-northeast-1 result.log $PAYLOAD
--- app/shells/localstack/update-invoke-localstack.sh
+++ app/shells/localstack/update-invoke-localstack.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
+while (( $# > 0 ))
+do
+  case $1 in
+    --paylaod | --payload=*)
+      if [[ "$1" =~ ^--paylaod= ]]; then
+        PAYLOAD=$(echo $1 | sed -e 's/^--paylaod=//')
+      elif [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+        echo "'paylaod' requires an argument." 1>&2
+        exit 1
+      else
+        PAYLOAD="--payload $2"
+        shift
+      fi
+      ;;
+  esac
+  shift
+done
+
 ./shells/localstack/update-func-localstack.sh
-./shells/localstack/invoke-func-localstack.sh
+./shells/localstack/invoke-func-localstack.sh $PAYLOAD
